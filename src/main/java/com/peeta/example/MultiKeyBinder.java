2848d3b2c4da437dca80d27d9ac14cd17a562137
Pietro Bellizio <pietrobellizio@iMac-di-Pietro.lan>
2024-10-20 14:06:24 +0200

    Initial commit

---
+++ src/main/java/com/peeta/example/MultiKeyBinder.java
@@ -0,0 +1,16 @@
+package com.peeta.example;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MultiKeyBinder implements ModInitializer {
+	public static final String MOD_ID = "multi-keybinder";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/com/peeta/example/MultiKeyBinderClient.java
@@ -0,0 +1,10 @@
+package com.peeta.example;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MultiKeyBinderClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
